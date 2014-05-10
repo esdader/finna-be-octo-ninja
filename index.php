19c561d25813f8ee8330e6f30f3a92b3ed45cad6
Tristan Ader <tristanader@gmail.com>
2014-05-10 16:10:30 -0500

    added sidebar panels and buttons

--- index.php
+++ index.php
@@ -10,6 +10,28 @@
             <li><a href="#add-link" class="int-nav-item">Tour Info</a></li>
             <li><a href="#add-link" class="int-nav-item">Upcoming</a></li>
         </ul>
+
+        <div class="l-sidebar-panel">
+            <header class="l-sidebar-panel-header">
+                <h3 class="sidebar-panel-heady">
+                    Book Your Event
+                </h3>
+            </header>
+            <div class="l-sidebar-panel-body">
+                <a href="#book-your-event" class="btn book-event-btn">Book Now</a>
+            </div>
+        </div>
+
+        <div class="l-sidebar-panel">
+            <header class="l-sidebar-panel-header">
+                <h3 class="sidebar-panel-heady">
+                    Sign Up!
+                </h3>
+            </header>
+            <div class="l-sidebar-panel-body">
+                
+            </div>
+        </div>
     </div>
     <div class="l-main-content">
         bar
