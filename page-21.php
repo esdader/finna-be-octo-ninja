dd4c0187abd7060ba06ce3ae94e82eb2f1803e48
Tristan Ader <tristanader@gmail.com>
2014-07-11 00:08:08 -0500

    the source is editable

--- page-21.php
+++ page-21.php
@@ -14,157 +14,32 @@
         
     <div class="l-main-content">
         <article id="the-source" class="l-int-panel int-panel-intro the-legend-panel">
-            <header>
-                <h1>The Source</h1>
+            <header><?php
+                if ( get_field('intro_heading') ) : ?>
+                    <h1><?php echo the_field('intro_heading'); ?></h1>
+                <?php endif; ?>
             </header>
-            <section>
-                <p>Like a great song, the source of great beer is a many splendored and illustriously elusive thing. Part inspiration, part imagination, part carbonation. Where does it all come from?</p>
-                
-                <p>To answer metaphysically, Liquid Austin Soul flows from the generations of groovy tunes and good times that have soaked the hallowed Hill Country since time began. (Or at least since the 1970s.).</p>
-
-                <p>To answer artisanally, the source of our beer is the collaborative genius of our two brewers. The Beatles had Lennon &amp; McCartney. Aerosmith has Tyler &amp; Perry. We’ve got Eric &amp; Russ. Long time compadres, brothers in brewing, it’s their vision you taste in every sip.</p>
-
-                <p>To answer pragmatically, if you wanna tune your palate to our frequency, the source is an ever growing number of stores, bars and restaurants around Austin.</p>
+            <section><?php
+                if ( get_field('intro_subheading') ) : ?>
+                    <h2 class="int-panel-tag"><?php echo the_field('intro_subheading'); ?></h2>
+                <?php endif;
+                if ( get_field('intro_main_content') ) : ?>
+                    <?php echo the_field('intro_main_content'); ?>
+                <?php endif; ?>
             </section>
         </article>
         <article id="stores" class="l-int-panel int-panel the-legend-panel">
-            <header>
-                <h1>The Stores</h1>
+            <header><?php
+                if ( get_field('stores_heading') ) : ?>
+                    <h1><?php the_field('stores_heading'); ?></h1>
+                <?php endif; ?>
             </header>
-            <section>
-                <p>
-                    Whip In <span class="bully">&bull;</span>
-
-                    Whichcraft Beer Store <span class="bully">&bull;</span>
-
-                    Alamo Drafthouse-Lakeline <span class="bully">&bull;</span>
-
-                    Red’s Porch <span class="bully">&bull;</span>
-
-                    Shiner’s Saloon <span class="bully">&bull;</span>
-
-                    Porter’s Gastropub <span class="bully">&bull;</span>
-
-                    Live Oak Market <span class="bully">&bull;</span>
-
-                    Texan Market #5 <span class="bully">&bull;</span>
-
-                    Snack Bar <span class="bully">&bull;</span>
-
-                    Brew Exchange <span class="bully">&bull;</span>
-
-                    Cain &amp; Abel’s <span class="bully">&bull;</span>
-
-                    The Woodland <span class="bully">&bull;</span>
-
-                    Gypsy Lounge <span class="bully">&bull;</span>
-
-                    Rosedale Market <span class="bully">&bull;</span>
-
-                    St. Roch’s Bar <span class="bully">&bull;</span>
-
-                    Sweetwater <span class="bully">&bull;</span>
-
-                    Angels Icehouse <span class="bully">&bull;</span>
-
-                    Junior’s Beer &amp; Wine <span class="bully">&bull;</span>
-
-                    Nasty’s <span class="bully">&bull;</span>
-
-                    One 2 One Bar <span class="bully">&bull;</span>
-
-                    West Lynn Food Mart <span class="bully">&bull;</span>
-
-                    Beverage World <span class="bully">&bull;</span>
-
-                    Sahara Lounge <span class="bully">&bull;</span>
-
-                    East Side Market <span class="bully">&bull;</span>
-
-                    Sunrise Mini Mart <span class="bully">&bull;</span>
-
-                    South Lamar Wine &amp; Spirits <span class="bully">&bull;</span>
-
-                    High Country Market <span class="bully">&bull;</span>
-
-                    Southwest Parkway Market <span class="bully">&bull;</span>
-
-                    King Liquor #3 <span class="bully">&bull;</span>
-
-                    Burnet Chevron <span class="bully">&bull;</span>
-
-                    River City Market <span class="bully">&bull;</span>
-
-                    Cris’ Liquor <span class="bully">&bull;</span>
-
-                    MLK Food Store <span class="bully">&bull;</span>
-
-                    Brodie Park Market <span class="bully">&bull;</span>
-
-                    1st Food Mart <span class="bully">&bull;</span>
-
-                    Hamrick’s Market <span class="bully">&bull;</span>
-
-                    V &amp; V Liquor <span class="bully">&bull;</span>
-
-                    Craig O’s Pizza &amp; Pasta <span class="bully">&bull;</span>
-
-                    Tejas Liquor <span class="bully">&bull;</span>
-
-                    Wells Mini-Mart <span class="bully">&bull;</span>
-
-                    Barton Food Mart <span class="bully">&bull;</span>
-
-                    QP Food Mart <span class="bully">&bull;</span>
-
-                    Fresh Plus <span class="bully">&bull;</span>
-
-                    Payless Food Store <span class="bully">&bull;</span>
-
-                    Minis Food Mart <span class="bully">&bull;</span>
-
-                    East First Grocery <span class="bully">&bull;</span>
-
-                    East Braker Lane Food Mart <span class="bully">&bull;</span>
-
-                    Barton Hill Food Mart <span class="bully">&bull;</span>
-
-                    Mel’s Lone Star Lanes <span class="bully">&bull;</span>
-
-                    Bread Basket <span class="bully">&bull;</span>
-
-                    Mets Mart <span class="bully">&bull;</span>
-
-                    Jet Spot <span class="bully">&bull;</span>
-
-                    Double Dave’s <span class="bully">&bull;</span>
-
-                    QQ Mart <span class="bully">&bull;</span>
-
-                    Heatherwild Food Mart <span class="bully">&bull;</span>
-
-                    Monarch Food Spot <span class="bully">&bull;</span>
-
-                    Dirty Dog <span class="bully">&bull;</span>
-
-                    ABC Liquor <span class="bully">&bull;</span>
-
-                    Speedi Stop <span class="bully">&bull;</span>
-
-                    Pecan Liquor <span class="bully">&bull;</span>
-
-                    Star Stop <span class="bully">&bull;</span>
-
-                    Family Grocery <span class="bully">&bull;</span>
-
-                    Congress Express <span class="bully">&bull;</span>
-
-                    Homefield Grill <span class="bully">&bull;</span>
-
-                    Cody’s Bistro and Lounge <span class="bully">&bull;</span>
-
-                    …And at select Twin Liquors and Walgreens
-                </p>
+            <section class="stores-list clearfix">
+                <?php 
+                    if ( get_field('stores_panel') ) {
+                        the_field('stores_panel');
+                    } 
+                ?>
             </section>
         </article>
     </div>    
