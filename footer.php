20eff7fd8eb0e060736b96384031c87540397d82
Tristan Ader <tristanader@gmail.com>
2014-08-07 17:14:44 -0500

    first pass footer

--- footer.php
+++ footer.php
@@ -1,3 +1,27 @@
+    <footer class="l-main-footer clearfix">
+        <div class="l-footer-nav clearfix">
+            <h3 class="footer-nav-heady">Social Responsibility</h3>
+            <ul class="l-horizontal-nav">
+                <li class="footer-facebook">
+                    <a href="https://www.facebook.com/SouthAustinBrewery">
+                        <span class="social-media-icon facebook ir"> </span>Facebook
+                    </a>
+                </li>
+                <li class="footer-twitter">
+                    <a href="https://twitter.com/SABrewery">
+                        <span class="social-media-icon twitter ir"> </span>
+                        Twitter
+                    </a>
+                </li>
+                <li class="footer-instagram">
+                    <a href="http://instagram.com/southaustinbrewery">
+                        <span class="social-media-icon instagram ir"> </span>
+                        Instagram
+                    </a>
+                </li>
+            </ul> 
+        </div>
+    </footer>
     </div> <?php // ends l-container ?>
     <?php wp_footer(); ?>
     
